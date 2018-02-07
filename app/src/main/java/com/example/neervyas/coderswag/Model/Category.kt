6b8abd55eb3be51cb3bcb51aca511407015249c4
neer-nab <neerajk207@gmail.com>
2018-02-07 17:59:59 +0530

    DataService and adapter Added

--- app/src/main/java/com/example/neervyas/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/neervyas/coderswag/Model/Category.kt
@@ -4,4 +4,8 @@ package com.example.neervyas.coderswag.Model
  * Created by neervyas on 7/2/18.
  */
 class Category(val  title: String, val image: String) {
+
+    override fun toString(): String {
+        return title
+    }
 }
